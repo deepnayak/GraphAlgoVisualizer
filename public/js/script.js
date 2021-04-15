1e28d91eadb2c9c6ffaa0218329d65822b4f7995
Deep <deepmumbai16@gmail.com>
2021-04-16 01:41:55 +0530

    Added Scribble functionality with custom color option

--- public/js/script.js
+++ public/js/script.js
@@ -22,10 +22,69 @@ var currEdgeLayer = new Layer();
 var drawLayer = new Layer();
 var speed = 1;
 var input = 1;
+var draw = 0;
+var currColor = 'white';
 // const sleep = (milliseconds) => {
 //   return new Promise(resolve => setTimeout(resolve, milliseconds))
 // }
+$('#checkbox').change(function() {
+  if(this.checked) {
+      draw = 1;
+      drawLayer.activate();
+      var textItem = new PointText(new Point(20, 30));
+      textItem.fillColor = 'white';
+      textItem.content = 'Click and drag to draw a line.';
+      $("#colorPicker").css("visibility", "visible");
+      $("#colorPickerl").css("visibility", "visible");
+  }
+  else {
+    $("#colorPicker").css("visibility", "hidden");
+    $("#colorPickerl").css("visibility", "hidden");
+    drawLayer.removeChildren();
+    draw = 0;
+  }
+  // $('#textbox1').val(this.checked);        
+});
 
+$('#colorPicker').change(function() {
+  currColor = this.value;
+})
+
+var path;
+
+
+// function onMouseDown(event) {
+// }
+
+function onMouseDrag(event) {
+	// Every drag event, add a point to the path at the current
+	// position of the mouse:
+  if(draw) {
+    path.add(event.point);
+    path.fullySelected = false;
+    // textItem.content = 'Segment count: ' + path.segments.length;
+  }
+}
+
+function onMouseUp(event) {
+  if(draw) {
+    	// When the mouse is released, simplify it:
+    path.simplify();
+    
+    // Select the path, so we can see its segments:
+    path.fullySelected = false;
+
+  }
+	// var segmentCount = path.segments.length;
+	
+
+	
+	// var newSegmentCount = path.segments.length;
+	// var difference = segmentCount - newSegmentCount;
+	// var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
+	// textItem.content = difference + ' of the ' + segmentCount + ' segments were removed. Saving ' + percentage + '%';
+
+}
 
 
 function drawOnCanvas () {
@@ -62,8 +121,45 @@ function createSlider(slider) {
 $(function() {
    createSlider($( "#slider-1" ));   
 });
+// $(document).ready(function() {
+//   $("#togglePaletteOnly").spectrum({
+//     showPaletteOnly: true,
+//     togglePaletteOnly: true,
+//     togglePaletteMoreText: 'more',
+//     togglePaletteLessText: 'less',
+//     color: 'blanchedalmond',
+//     palette: [
+//         ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
+//         ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
+//         ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
+//         ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
+//         ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
+//         ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
+//         ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
+//         ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+//     ]
+//   });
+// });
 
 function onMouseDown(event) {
+
+  if(draw) {
+    drawLayer.activate();
+    // If we produced a path before, deselect it:
+    if (path) {
+      path.selected = false;
+    }
+
+    path = new Path();
+    path.strokeColor = currColor;
+    path.strokeWidth = 3;
+    
+    // Select the path, so we can see its segment points:
+    path.fullySelected = true;
+    return;
+  }
+
+
   if(!event.modifiers.shift) {
     verticesLayer.activate();
     path = new Path();
